f9f232fde3be4ae615c7ef5e071e526cba631cc8
kq1158 <kq1158@gmail.com>
2021-07-15 22:27:06 +0530

    Day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/pages/home_page.dart';
+// import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
@@ -7,21 +8,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       initialRoute: "/home",
       routes: {
-        "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        // "/": (context) => LoginPage(),
+        // "/home": (context) => HomePage(),
+        "/home": (context) => LoginPage()
       },
     );
   }
